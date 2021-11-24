9c3860cfe2a9d19b7e57bcde223ad832e43dff71
Krystian Usarz <Krystian.Usarz@sage.com>
2021-11-24 16:30:44 +0100

    here safe from day one

---
+++ blog-schreiben/schreiben.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset='utf-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <title>Krystians Blog</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <link rel="stylesheet" href="../vorlage/blog-vorlage.css">
+        <link rel="stylesheet" href="schreiben-style.css">
+    </head>
+
+    <body>
+        <?php include '../vorlage/blog-vorlage.php';?>
+
+        <div class="layoutMessageBoxes">
+            <div class="messageBox">
+                <h4 class="userName">Krystian<br><br></h4>
+                <p class="">hallo zusammen dies ist mein erster Blogartikel ich hoffe mein 
+                    Kontent wird euch gefallen :)
+                </p>
+            </div>
+        </div>
+    </body>
+
+</html>
--- homepage/index.php
+++ homepage/index.php
@@ -1,19 +1,16 @@
+
 <?php
 
-$user = 'admin';
-$password = 'Depole2005?';
+$user = 'root';
+$password = ''  ;
 $database = 'blog';
 
-$pdo = new PDO('mysql:host=localhost;dbname= ' . $database, $user, $password, [
+$pdo = new PDO('mysql:host=localhost;dbname=' . $database, $user, $password, [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 ]);
 
-$stmt = $pdo->query('SELECT creator FROM `blogs`');
-foreach($stmt->fetchAll() as $task) {
-    var_dump($task);
-}
 ?>
 
 <!DOCTYPE html>
@@ -33,7 +30,16 @@ foreach($stmt->fetchAll() as $task) {
 
         <div class="layoutMessageBoxes">
             <div class="messageBox">
-                <h4 class="userName">Krystian<br><br></h4>
+            <?php
+
+                $stmt = $pdo->prepare('SELECT * FROM `blog` WHERE creator !!=');
+                $stmt->execute([':ID' => 0]);
+                
+                foreach($stmt->fetchAll() as $x) {
+                    var_dump($x);
+                }
+                
+            ?>
                 <p class="blogText">hallo zusammen dies ist mein erster Blogartikel ich hoffe mein 
                     Kontent wird euch gefallen :)
                 </p>
